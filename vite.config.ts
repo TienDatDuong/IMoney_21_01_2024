f3e06869e788c32c83e280d6e9abce36e3644607
Duong Tien Dat <duongtiendat3103@gmail.com>
2024-05-01 10:26:21 +0700

    config vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import vue from "@vitejs/plugin-vue";
-import { resolve, _dirname } from "node:path";
+import { resolve } from "node:path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
