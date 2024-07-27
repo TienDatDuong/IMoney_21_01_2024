049ca54c2e9f9112097e794e9b97c4316fbf1158
Duong Tien Dat <duongtiendat3103@gmail.com>
2024-07-28 00:01:10 +0700

    add component infouser

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "./style/tailwind.css";
 import "./style.css";
 import App from "./App.vue";
 import router from './router'
+import store from "./store";
 import { registerComponentGlobal } from "./utils/import";
 
 
@@ -11,5 +12,6 @@ const app = createApp(App)
 registerComponentGlobal(app)
 
 app.use(router)
+app.use(store)
 
 app.mount("#app");
